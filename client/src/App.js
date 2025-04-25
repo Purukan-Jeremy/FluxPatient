bd9a84125878ff6f7d90a76b36938fc0a919f01a
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2025-04-25 19:43:59 +0800

    sambung front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((response) => response.json())
+      .then((data) => setBackendData(data))
+      .then((data) => console.log(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof backendData.users === "undefined") ?(
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>
+            {user}</p>
+        ))
+      )}
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const usersController = require("./app/user/user.controller");
 
@@ -14,5 +15,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", usersController);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+app.use(cors());
 
 module.exports = app;
