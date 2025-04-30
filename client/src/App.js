bedf4adb312626b7f5d306a394261ebcc601434b
FaithzaMandolang <s22210337@student.unklab.ac.id>
2025-04-30 21:26:22 +0800

    edit doctors

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import Database from "./config/database/index.js";
 import "./App.css";
 import Dashboard from "./components/Dashboard/index.js";
+import Doctors from "./components/Doctors/index.js";
 function App() {
-
   return (
     <div>
-      <Dashboard/>
-      <Database/>
+      <Doctors />
     </div>
   );
 }
