0990b9822b34ddd81cd76e34933eb15c9a099c99
gracealynn <s22210508@student.unklab.ac.id>
2025-05-01 02:08:16 +0800

    Queue & Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Database from "./config/database/index.js";
 import "./App.css";
-import Dashboard from "./components/Navbar/index.js";
-import Home from "./components/Home/index.js";
-import SymptomChecker from "./components/Symptom/index.js";
+// import Dashboard from "./components/Navbar/index.js";
+// import Home from "./components/Home/index.js";
+// import SymptomChecker from "./components/Symptom/index.js";
+import Queue from "./components/Queue/index.js";
 function App() {
   return (
     <div>
       {/* <Dashboard/> */}
-      <SymptomChecker/>
+      <Queue />
       {/* <Home/> */}
-      <Database/>
+      <Database />
     </div>
   );
 }
